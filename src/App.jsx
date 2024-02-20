0207a07924b5f9f14410117910b71f4c3f829df7
AykhanMahmudzade <ayxan.mahmudzade@mail.ru>
2024-02-20 23:19:48 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+import './App.css'
+
+// * Komponentler gelecek
+
+import NavbarKomponenti from './components/Navbar'
+
+
+function App() {
+
+  return (
+    < NavbarKomponenti />
+  )
+}
+
+export default App
